74e0244d8daa9f8a6e6a6f53e15af6d071e2ebf5
Jerome <jeromelesne@gmail.com>
2025-04-17 11:43:19 +0200

    add swagger

--- cesiZen-back/src/main/java/com/cesi/cesiZen/controller/UserController.java
+++ cesiZen-back/src/main/java/com/cesi/cesiZen/controller/UserController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cesi.cesiZen.entity.User;
 import com.cesi.cesiZen.service.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 public class UserController {
     private UserService userService;
@@ -18,12 +20,14 @@ public class UserController {
     }
 
     @GetMapping("/users")
+    @Operation(summary = "Get all users", description = "Returns a list of all users")
     public List<User> findAll() {
         List<User> usersList = userService.findAll();
         return usersList;
     }
 
     @GetMapping("/users/{id}")
+    @Operation(summary = "Get user by id", description = "Ruturns a User")
     public User findById(@PathVariable Long id) {
         User user = userService.findById(id);
         return user;
